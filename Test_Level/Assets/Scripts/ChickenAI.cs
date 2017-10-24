c6c3c0390471e929473fb09179a7837b8e1a3298
Unknown <kendomorris@yahoo.com>
2017-10-23 20:09:34 -0600

    Edits on the game

---
+++ Test_Level/Assets/Scripts/ChickenAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenAI : MonoBehaviour {
+	public Rigidbody enemy;
+	public float moveSpeed;
+	public Transform target;
+	public Transform target2;
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player"){
+			Debug.Log("Player has entered chickens trigger");
+			transform.LookAt(target);
+			transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+			
+			}
+			if(other.gameObject.name == "Wolf"){
+			Debug.Log("Wolf has entered chickens trigger");
+			transform.LookAt(target2);
+			transform.Translate(Vector3.back*moveSpeed*Time.deltaTime);
+			}
+		}
+
+
+
+	}
+	
+		
+	
+
---
+++ Test_Level/Assets/Scripts/WolfAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfAI : MonoBehaviour {
+
+	public Transform target;
+	public Transform target2;
+	public float moveSpeed;
+	void OnTriggerStay(Collider other)
+	{
+		if(other.gameObject.name == "Player"){
+			Debug.Log("Player has entered Wolf's Trigger");
+			transform.LookAt(target);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+		if(other.gameObject.name == "Chicken"){
+			Debug.Log("Chicken has entered Wolf's Trigger");
+			transform.LookAt(target2);
+			transform.Translate(Vector3.forward*moveSpeed*Time.deltaTime);
+		}
+	}
+
+}
